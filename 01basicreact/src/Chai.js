58c87e91cab39750a353f55b0e372c88eb13c4b5
Shreyansh <258004892+Shreyansh-gith@users.noreply.github.com>
2026-07-07 14:30:20 +0530

    Remove files from tracking but keep them locally

--- 01basicreact/src/Chai.js
+++
@@ -1,8 +0,0 @@
-function Chai(){
-    return (
-        <h3>Chai is Ready</h3>
-    );
-}
-// Create React requires the function names to start with a capital letter
-
-export default Chai
--- 01vitereact/src/chai.jsx
+++
@@ -1,9 +0,0 @@
-function Chai(){
-    return (
-        <h3>Chai is ready</h3>
-    )
-}
- 
-// Vite requires the function names to start with a capital letter
-
-export default Chai
